7a93969200e01e98acbd269886cca5b33557b372
Jahir007 <68056943+Jahir007@users.noreply.github.com>
2020-09-26 20:11:20 +0530

    Update script.js

--- Module 4/script.js
+++ Module 4/script.js
@@ -1,19 +1,35 @@
-(function (window) {
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+*/
 
 
-var names = ["Jahir", "Khan", "Zishan", "Jay", "Paul", "Frank", "Larry", "Paula", "Laura", "loba"];
+(function () {
 
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i in names) {
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
-
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-
-
-  if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
-  } else {
-     window.helloSpeaker.speak(names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
   }
 }
-})(window);
+
+})();
